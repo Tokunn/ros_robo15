c693ed4219ef13f0e5b080b0ca64008f234d6041
Tokunn <you@example.com>
2015-08-25 15:58:00 +0900

    joystick.h

--- src/gamepad_cpp.cpp
+++ src/gamepad_cpp.cpp
@@ -3,21 +3,67 @@
 
 #include <sstream>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "joystick.h"
+
+#define BUTTON_DATA_MAX 12
+#define STICK_DATA_MAX 6
+#define MAX_STICK 1 
+#define BUTTON_X 0
+#define BUTTON_A 1
+#define BUTTON_B 2
+#define BUTTON_Y 3
+#define BUTTON_LB 4
+#define BUTTON_RB 5
+#define BUTTON_LT 6
+#define BUTTON_RT 7
+#define BUTTON_BACK 8
+#define BUTTON_START 9
+#define BUTTON_STICKLEFT 10
+#define BUTTON_STICKRIGHT 11
+#define LEFT_STICK_X 0
+#define LEFT_STICK_Y 1
+#define RIGHT_STICK_X 2
+#define RIGHT_STICK_Y 3
+#define CROSS_STICK_X 4
+#define CROSS_STICK_Y 5
 
 
 /*----- main() -----*/
 int main(int argc, char **argv) {
     ros::init(argc, argv, "gamepad_cpp");
 
+    int fd = open("/dev/input/js0", O_RDONLY);
+    unsigned char button_data[BUTTON_DATA_MAX];
+    float stick_data[STICK_DATA_MAX];
+
     ros::NodeHandle n;
 
     ros::Publisher gamepad_cpp_pub = n.advertise<ros_robo15::Gamepad_cmd>("gamepad_cmd", 1000);
 
     ros::Rate loop_rate(50);
+    
+    ros_robo15::Gamepad_cmd gamepad_cmd;
 
     while (ros::ok()) {
 
-        ros_robo15::Gamepad_cmd gamepad_cmd;
+        struct js_event event;
+        if (read( fd, &event, sizeof(struct js_event) ) >= sizeof(struct js_event) ) {
+            switch (event.type & 0x7f) {
+                case JS_EVENT_BUTTON:
+                    if (event.number < BUTTON_DATA_MAX) {
+                        button_data[event.number] = event.value;
+                    }
+                    break;
+                case JS_EVENT_AXIS:
+                    if (event.number < STICK_DATA_MAX) {
+                        stick_data[event.number] = event.value;
+                    }
+                    break;
+            }
+        }
 
         gamepad_cpp_pub.publish(gamepad_cmd);
 
