bbdb420cec3ac563f950e6e392e6ae81b09a8624
Tokunn <you@example.com>
2015-08-25 16:36:29 +0900

    update

--- src/gamepad_cpp.cpp
+++ src/gamepad_cpp.cpp
@@ -65,6 +65,8 @@ int main(int argc, char **argv) {
         
         ros_robo15::Gamepad_cmd gamepad_cmd;
 
+        ROS_INFO("left axis x %d", stick_data[LEFT_STICK_X];
+
         gamepad_cmd.left_axis_x = 2;//stick_data[LEFT_STICK_X];
         gamepad_cmd.left_axis_y = 2;//stick_data[LEFT_STICK_Y];
         gamepad_cmd.right_axis_x = 2;//stick_data[RIGHT_STICK_X];
