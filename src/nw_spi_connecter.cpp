7182a008925c4953e31d6fa32dc55b828b179da0
Tokunn <you@example.com>
2015-08-29 14:55:51 +0900

    udp

--- src/nw_spi_connecter.cpp
+++ src/nw_spi_connecter.cpp
@@ -4,8 +4,23 @@
 #include <sstream>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 void send_udp(const ros_robo15::Spi_cmd::ConstPtr& txbuf_msg) {
     ROS_DEBUG("nw send 0x%x", txbuf_msg->spi_cmd);
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4000);
+    addr.sin_addr.s_addr = inet_addr("localhost");
+
+
+    sendto(sock, &txbuf_msg->spi_cmd, 1, 0, (struct sockaddr *)&addr, sizeof(addr));
+
+    close(sock);
 }
 
 int main(int argc, char **argv) {
