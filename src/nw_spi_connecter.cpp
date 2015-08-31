812efd2f5fa23a124d8e9ccd30ab11c5bddcfc32
Tokunn <you@example.com>
2015-08-31 18:56:27 +0900

    ip change

--- src/nw_spi_connecter.cpp
+++ src/nw_spi_connecter.cpp
@@ -9,13 +9,15 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define IP_ADDR "172.16.14.104"
+
 void send_udp(const ros_robo15::Spi_cmd::ConstPtr& txbuf_msg) {
     ROS_DEBUG("nw send 0x%x", txbuf_msg->spi_cmd);
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(4000);
-    addr.sin_addr.s_addr = inet_addr("172.16.14.224");
+    addr.sin_addr.s_addr = inet_addr(IP_ADDR);
 
 
     sendto(sock, &txbuf_msg->spi_cmd, 1, 0, (struct sockaddr *)&addr, sizeof(addr));
