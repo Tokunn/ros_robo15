511369ccc06b64ba45deee5347b31a05972b125f
Tokunn <you@example.com>
2015-10-03 20:08:32 +0900

    include

--- include/wiringspi.h
+++ include/wiringspi.h
@@ -1,3 +1,4 @@
-int wiringPiSPIDataRW(int channel, unsigned char *txbuf, unsigned char *rxbuf, int len);
+int wiringPiSPIDataRW(int channel, const unsigned char *txbuf,
+        unsigned char *rxbuf, int len);
 int wiringPiSPISetupMode(int channel, int speed, int mode);
 int wiringPiSPISetup(int channel, int speed);
