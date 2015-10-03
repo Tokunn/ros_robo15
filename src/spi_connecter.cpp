a30efdda01080cfba538e45f85e4d3895948e385
Tokunn <you@example.com>
2015-10-03 17:32:29 +0900

    new spi_connecter.cpp

--- src/spi_connecter.cpp
+++ src/spi_connecter.cpp
@@ -4,55 +4,31 @@
 #include <sstream>
 #include <stdlib.h>
 
-#include "libspi.h"
-#include "wiringPiSPI.h"
-
 #define PACKET_SIZE_BYTE 1
 #define BUS 0
 
 #define DEFAULT_CS 0
 
-#define RPI
-
 class SpiRosTransfer
 {
     public:
-        SpiRosTransfer(int p_cs) {
+        SpiRosTransfer(int tmp_cs) {
             this->pub = this->n.advertise<ros_robo15::Spi_cmd>("recive_data", 1000);
-            this->sub = this->n.subscribe("send_data", 1000, &SpiRosTransfer::spi_transfer, this);
-
-            this->cs = p_cs;
-            this->fd = wiringPiSPISetup(this->cs, 500000);
-            if (fd < 0) { return; }
+            this->sub = this->n.subscribe("send_data", 1000,
+                    &SpiRosTransfer::spi_transfer, this);
+            this->cs = tmp_cs;
         }
-
 
     private:
         ros::NodeHandle n;
-
         ros::Publisher pub;
         ros::Subscriber sub;
 
-        void spi_transfer(const ros_robo15::Spi_cmd::ConstPtr& txbuf_msg) {
-            ROS_INFO("spi_transfer start");
-            uint8_t buf_msg = txbuf_msg->spi_cmd;
-
-            ROS_INFO("wiringPiSPIDataRW start");
-            wiringPiSPIDataRW(this->cs, &buf_msg, PACKET_SIZE_BYTE); 
-            ROS_INFO("wiringPiSPIDataRW end");
-
-            ros_robo15::Spi_cmd rxbuf_msg;
-            rxbuf_msg.spi_cmd = buf_msg;
-            ROS_DEBUG("transfer data: Send[0x%x] Recive[0x%x]",
-                    txbuf_msg->spi_cmd, rxbuf_msg.spi_cmd);
-            this->pub.publish(rxbuf_msg);
-            ros::spinOnce();
-            ROS_INFO("spi_transfer end");
-        }
-
-        int fd;
         int cs;
 
+        void spi_transfer() {
+            ros::spinOnce();
+        }
 };
 
 int main(int argc, char **argv)
@@ -64,12 +40,8 @@ int main(int argc, char **argv)
     if (argc > 1) {
         cs = atoi(argv[1]);
         ROS_INFO("SPI_CS : %d", cs);
-    } else {
-        ROS_WARN("Not given arguments");
     }
-
     SpiRosTransfer spirostransfer(cs);
-
     ros::spin();
 
     return EXIT_SUCCESS;
