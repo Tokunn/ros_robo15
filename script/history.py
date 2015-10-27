8e089ad9ba815ab153afdc873f1f62dd69165607
Tokunn <you@example.com>
2015-10-27 21:40:32 +0900

    history

--- script/history.py
+++ script/history.py
@@ -5,8 +5,9 @@
 
 import rospy
 from ros_robo15.msg import Gamepad_cmd
+from ros_robo15.msg import Spi_cmd
 
-def callback(data):
+def button(data):
     if (str(data.button_x) == 'True'):
         rospy.loginfo(rospy.get_caller_id() + " X TRI")
     if (str(data.button_a) == 'True'):
@@ -14,10 +15,19 @@ def callback(data):
     if (str(data.button_b) == 'True'):
         rospy.loginfo(rospy.get_caller_id() + " B SNIP")
 
+def spi_data(data):
+    if (data.spi_cmd == 24):
+        rospy.loginfo(rospy.get_caller_id() + " SPI : X")
+    if (data.spi_cmd == 40):
+        rospy.loginfo(rospy.get_caller_id() + " SPI : A")
+    if (data.spi_cmd == 56):
+        rospy.loginfo(rospy.get_caller_id() + " SPI : B")
+
 def history_node():
     rospy.init_node('history_node', anonymous=False)
 
-    rospy.Subscriber("/gamepad_cmd", Gamepad_cmd, callback)
+    rospy.Subscriber("gamepad", Gamepad_cmd, button)
+    rospy.Subscriber("send_data", Spi_cmd, spi_data)
 
     rospy.spin()
 
